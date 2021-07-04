afb963497094724b3868ca1fdd82fb405045400d
Tianyi Zhou <zcarol.530@gmail.com>
2021-07-04 01:26:41 -0400

    added Handler for Number Format Exception

--- src/test/java/com/example/springrecipeapp/controllers/RecipeControllerTest.java
+++ src/test/java/com/example/springrecipeapp/controllers/RecipeControllerTest.java
@@ -105,4 +105,12 @@ class RecipeControllerTest {
                 .andExpect(status().isNotFound())
                 .andExpect(view().name("404error"));
     }
+
+    @Test
+    public void testGetRecipeNumberFormatException() throws Exception {
+
+        mockMvc.perform(get("/recipe/adfadf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("400error"));
+    }
 }
